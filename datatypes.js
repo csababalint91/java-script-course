3e3cd44e2984d2108522815b983c651907a843a2
csababalint91 <40868013+csababalint91@users.noreply.github.com>
2019-02-25 13:04:55 +0000

    Datatypes part 2

---
+++ datatypes.js
@@ -0,0 +1,16 @@
+/*
+let x = 7;
+console.log(typeof x);
+
+let y = false; // true or false
+console.log(typeof y);
+
+let z = ('Hellow World!!'); // single quotes
+console.log(typeof z);
+*/
+
+let a;
+console.log(a);
+console.log(typeof a);
+
+console.log('9');
