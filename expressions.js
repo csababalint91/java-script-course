67032770dbf61fdbb75d5955b1f3bfac3b0cd379
csababalint91 <40868013+csababalint91@users.noreply.github.com>
2019-02-26 14:41:22 +0000

    Expressions and Operators

---
+++ expressions.js
@@ -0,0 +1,25 @@
+// This is bad
+// a;
+
+
+// Types of expressions
+// ===================================
+
+// 1. Variable declaration
+// let = a;
+
+// 2. Asign a value
+
+// a = 4;
+
+// Perform an evaluation that returns a single value
+// b + c
+
+let b = 3;
+let c = 2;
+// Three expressions here....can you find them?
+let a = b + c;
+
+// 1. let a... variable declaration
+// 2. perform an eval b + c
+// 3. result assigned to a
