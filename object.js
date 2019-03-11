62be40f3deefdebe1210be08118314db4e647738
csababalint91 <40868013+csababalint91@users.noreply.github.com>
2019-03-11 17:53:41 +0000

    The 'this' keyword

--- object.js
+++ object.js
@@ -15,3 +15,43 @@ let car = {
 
 car.printDescription();
 console.log(car.year);
+
+
+console.log(car['year']);
+// NEVER DO THIS,CREATS A NEW INDEX // console.log(car[1]);
+ /*
+var anotherCar = {};
+anotherCar.whatever = 'bob';
+console.log(anotherCar.whatever);
+*/
+
+/*
+var a = {
+    myProperty: { b: 'hi'}
+};
+*/
+
+console.log(a.myProperty.b);
+
+
+//OBJECT GRAPH
+var c = {
+    myProperty: [
+        { d: 'this' },
+        { e: 'can' },
+        { f: 'get' },
+        { g: 'crazy' }
+    ],
+
+    myProperty2: [
+        { h: 'love' },
+        { i: 'is'},
+        { j: 'real'}
+    ]
+};
+
+let carlot = [
+    { year: 2017, make: 'Toyota', model: '4Runner'}, 
+    { year: 2015, make: 'BMW', model: '528i'}, 
+    { year: 1982, make: 'Buick', model: 'Skylark'} 
+];
