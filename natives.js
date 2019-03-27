7bdd804070d79a6c5b102bb24238a0ccf2c3aafb
csababalint91 <40868013+csababalint91@users.noreply.github.com>
2019-03-27 16:10:25 +0000

    Natives

---
+++ natives.js
@@ -0,0 +1,43 @@
+/* let pattern = xyz;
+let value = 'This is xyz test.';
+console.log(value.replace(pattern, 'just'));
+
+ */
+
+ let myString = new String('Howdy');
+ // console.log(myString);
+console.log(myString.toString());
+
+console.log(typeof myString);
+ console.log(myString);
+
+ /*
+ String()
+ Number()
+ Boolean()
+ Object()
+ Function()
+ Symbol()
+ */
+
+ /*
+ Array()
+ Regexp()
+ */
+
+ /*
+ Date()
+ Error()
+ */
+
+/*  let myPrimitive = 'THIS IS CRAZY';
+ console.log(typeof myPrimitive);
+ myPrimitive.toLowerCase();
+ console.log(typeof myPrimitive); */
+
+ let myNumber = new Number(7);
+ console.log(typeof myNumber);
+ let myPrimitive = myNumber.valueOf();
+ console.log(typeof myPrimitive);
+
+
