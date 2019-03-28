14cc114108f1491194420513d48b319b0bd2201b
csababalint91 <40868013+csababalint91@users.noreply.github.com>
2019-03-28 12:16:22 +0000

    Truthy and Falsy Values

---
+++ falsy-truthy.js
@@ -0,0 +1,9 @@
+if (false)    {} else { console.log('false is falsy'); }
+if (null)    {} else { console.log('null is falsy'); }
+if (undefined)    {} else { console.log('undefined is falsy'); }
+if (0)    {} else { console.log('0 is falsy'); }
+if (NaN)    {} else { console.log('NaN is falsy'); }
+if ('')    {} else { console.log('an empty string with single-quotes is empty'); }
+if ("")    {} else { console.log('an empty array with double-quotes is empty'); }
+
+// Everithing else is truthy
